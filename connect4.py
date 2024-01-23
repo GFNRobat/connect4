b5cbad72ff8227216dcb7ca68ecb9b760ec0fad0
GFNRobat <r.speiser.gfn@gmail.com>
2024-01-23 11:13:52 +0100

    play() funzt -> check() fehlt noch

--- connect4.py
+++ connect4.py
@@ -6,22 +6,57 @@ feld = [[" . "," . "," . "," . "," . "," . "," . "],
         [" . "," . "," . "," . "," . "," . "," . "],
         [" . "," . "," . "," . "," . "," . "," . "]]
 
-player = " X "
+regeln = [[" | "," | "," | "," | "," | "," | "," | "],
+        [" 1 "," 2 "," 3 "," 4 "," 5 "," 6 "," 7 "]]
 
-def draw():
-    for row in feld:
+player = " X "
+done = 0
+
+def reihe():
+    global player
+    if player == " X ":
+        player = " O "
+    else:
+        player = " X "
+
+def draw(liste):
+    for row in liste:
         print(*row, sep=" |")
 
 def spielzug(ein):
-    pass
+    global player
+    for i in range(1, 7):
+        if feld[-i][ein] == " . ":
+            feld[-i][ein] = player
+            reihe()
+            break
 
 def eingabe():
-    ein = input(f"Spieler {player} ist an der Reihe\nBitte Spielzug wählen")
-    if feld[ein][0] != " . ":
-        print("Feld belegt. Bitte anderen Spielzug wählen.")
+    draw(feld)
+    draw(regeln)
+    try:
+        ein = (int(input(f"Spieler {player} ist an der Reihe\nBitte Spielzug wählen\n"))-1)
+        if ein >= 0 and ein <= 6:
+            if feld[0][ein] != " . ":
+                print("Feld belegt. Bitte anderen Spielzug wählen.")
+                eingabe()
+            else:
+                spielzug(ein)
+        else:
+            print("Ungültige Eingabe. Bitte nur Zahlen zwischen 1 und 7 eingeben")
+    except:
+        print("Ungültige Eingabe. Bitte nur Zahlen zwischen 1 und 7 eingeben")
         eingabe()
-    else:
-        spielzug(ein)
 
-draw()
-eingabe()
+
+
+def check():
+    global done
+    pass
+
+def play():
+    while done == 0:
+        eingabe()
+        check()
+
+play()
