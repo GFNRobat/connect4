dd83870e1e7c798c90df1dab56c540c05029ffd8
GFNRobat <r.speiser.gfn@gmail.com>
2024-01-23 09:27:59 +0100

    eingabe() definiert

--- connect4.py
+++ connect4.py
@@ -6,10 +6,22 @@ feld = [[" . "," . "," . "," . "," . "," . "," . "],
         [" . "," . "," . "," . "," . "," . "," . "],
         [" . "," . "," . "," . "," . "," . "," . "]]
 
+player = " X "
+
 def draw():
     for row in feld:
         print(*row, sep=" |")
 
+def spielzug(ein):
+    pass
 
+def eingabe():
+    ein = input(f"Spieler {player} ist an der Reihe\nBitte Spielzug wählen")
+    if feld[ein][0] != " . ":
+        print("Feld belegt. Bitte anderen Spielzug wählen.")
+        eingabe()
+    else:
+        spielzug(ein)
 
 draw()
+eingabe()
